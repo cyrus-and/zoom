8190325d15150eb37e946a95d8c641739360b2aa
Andrea Cardaci <cyrus.and@gmail.com>
2017-10-06 13:21:21 +0200

    Improve package description

--- zoom-mode.el
+++ zoom-mode.el
@@ -27,8 +27,8 @@
 
 ;; This minor mode takes care of managing the window sizes by enforcing a fixed
 ;; and automatic balanced layout where the currently selected window is resized
-;; according to `zoom-size' which can be either an absolute value in
-;; lines/columns or a ratio between the selected window and frame size.
+;; according to `zoom-size` which can be an absolute value in lines/columns, a
+;; ratio between the selected window and frame size or even a custom callback.
 
 ;;; Code:
 
