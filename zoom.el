f05e898c56e5e8681eba8aa6544baf7af9542eda
Andrea Cardaci <cyrus.and@gmail.com>
2017-10-07 12:53:51 +0200

    Fix comment typo

--- zoom.el
+++ zoom.el
@@ -30,7 +30,7 @@
 
 ;; This minor mode takes care of managing the window sizes by enforcing a fixed
 ;; and automatic balanced layout where the currently selected window is resized
-;; according to `zoom-size` which can be an absolute value in lines/columns, a
+;; according to `zoom-size' which can be an absolute value in lines/columns, a
 ;; ratio between the selected window and frame size or even a custom callback.
 
 ;;; Code:
