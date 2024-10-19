f5f635e1fc5a8f606b7386286546bb6439e7124c
Andrea Cardaci <cyrus.and@gmail.com>
2024-10-19 23:01:32 +0200

    Bump version

--- zoom.el
+++ zoom.el
@@ -21,7 +21,7 @@
 ;; SOFTWARE.
 
 ;; Author: Andrea Cardaci <cyrus.and@gmail.com>
-;; Version: 0.2.4
+;; Version: 0.3.0
 ;; URL: https://github.com/cyrus-and/zoom
 ;; Package-Requires: ((emacs "24.4"))
 ;; Keywords: frames
